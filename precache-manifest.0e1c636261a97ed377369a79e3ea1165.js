f61f77097b59dd3bfe64e7a4683c532b13e33802
jung_kook <jung_kook@tmax.co.kr>
2020-08-13 20:05:07 +0900

    Updates

---
+++ precache-manifest.0e1c636261a97ed377369a79e3ea1165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "379bf8a586152f88750734933085d7d3",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "5afc5b153457b008223d",
+    "url": "/movie_app/static/css/main.b62c3136.chunk.css"
+  },
+  {
+    "revision": "a0054564cd7082ef0d3c",
+    "url": "/movie_app/static/js/2.59df1732.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app/static/js/2.59df1732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5afc5b153457b008223d",
+    "url": "/movie_app/static/js/main.676b7791.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
